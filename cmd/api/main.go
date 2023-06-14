6f0cdebbdb6fb1caa57ee64f42e2909bea97ac1e
Tomas Torres <tomastorres003@hotmail.com>
2023-06-13 23:45:18 -0300

    feat: add readme to explain the project

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/TomyPY/FinTracker/cmd/api/routes"
 	"github.com/gin-gonic/gin"
 )
+
+//
+// @title FinanceAPI
+// @version 0.1
+// @description Small project to manage your finances
+
+//@host localhost:8080
+//@BasePath /api/v1
 
 func main() {
 	if err := run(); err != nil {
@@ -15,10 +24,27 @@ func main() {
 
 func run() error {
 
+	//Configurate depedencies
+	// env := environment.GetFromString(os.Getenv("GO_ENVIRONMENT"))
+	// depend, err := dependencies.BuildDependencies(env)
+	// if err != nil {
+	// 	return err
+	// }
+
+	//Init db
+	// db, err := depend.Db.Init()
+	// if err != nil {
+	// 	return err
+	// }
+
+	var db *sql.DB
+
+	//Configurate routes
 	eng := gin.Default()
-	router := routes.NewRouter(eng)
+	router := routes.NewRouter(db, eng)
 	router.MapRoutes()
 
+	//Start server
 	if err := eng.Run(); err != nil {
 		return err
 	}
